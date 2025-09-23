cb16b62ac6775621d5b5cc2655288267709c5e54
aluno <etecae023@hotmail.com>
2025-09-23 15:22:23 -0300

    Atualização melhor

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import json
 data_json = "dados_rotina.json"
 
 st.write(
-    '<img src="https://i.pinimg.com/236x/11/0f/59/110f59578288d703475e31c3b22ad66c.jpg" width="80" style="border-radius: 10px; margin: 5px 0px 5px 0px; border: 1px solid #fff" />',
+    '<img src="https://i.pinimg.com/236x/11/0f/59/110f59578288d703475e31c3b22ad66c.jpg" width="70" style="border-radius: 6px; margin: 5px 0px 5px 0px;" />',
     unsafe_allow_html=True,
 )
 st.write(
-    '<span style="font-family: Arial; font-size: 34px; font-weight: 600;">Dashboard Pessoal - Python</span>',
+    '<span style="font-family: Arial; font-size: 34px; font-weight: 600;">- Dashboard Pessoal - Python</span>',
     unsafe_allow_html=True,
 )
 
@@ -59,12 +59,18 @@ if dados_existentes:
             )
         st.success("Alterações salvas com sucesso!")
 
+    st.write("<br><br>", unsafe_allow_html=True)
+
+    st.write(
+        '<span style="font-family: Arial; font-size: 34px; font-weight: 600;">🎯 Progresso das tarefas</span>',
+        unsafe_allow_html=True,
+    )
+
     selecionar = st.multiselect("Selecione para filtrar", df["concluida"].unique())
     if selecionar:
         df = df[df["concluida"].isin(selecionar)]
         st.write(df)
 
-    st.subheader("Progresso das Tarefas")
     contagem = df["concluida"].value_counts().reset_index()
     contagem.columns = ["Status", "Quantidade"]
 
@@ -72,21 +78,3 @@ if dados_existentes:
 
 else:
     st.write("Nenhuma tarefa adicionada ainda.")
-
-st.write(
-    '<hr style="width: 100%;height: 1px;border: 0px;border-top: 1px solid white;background-color: #fff;"><br>',
-    unsafe_allow_html=True,
-)
-st.write(
-    '<span style="font-family: Arial; font-size: 30px; font-weight: 600;">🎯 Visão de Agenda</span>',
-    unsafe_allow_html=True,
-)
-st.write("<br>", unsafe_allow_html=True)
-
-st.write(
-    "<span>Agenda:</span>",
-    unsafe_allow_html=True,
-)
-
-hora_selecionada = st.time_input("Selecione um horário")
-st.write("Horário selecionado:", hora_selecionada)
